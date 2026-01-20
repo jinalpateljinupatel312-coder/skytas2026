5cd79f7f0c3f563ca3e426dd6812659b78577cc7
jinalpateljinupatel312-coder <jinalpateljinupatel312@gmail.com>
2026-01-20 11:53:21 +0530

    Delete prog6.py

--- prog6.py
+++
@@ -1,7 +0,0 @@
-
-text = input("Enter a string:")
-letter = input("Enter the letter to count: ")
-
-count = text.count(letter)
-
-print(f" The letter '{letter}' appears {count} times in the string.")
