9d717da0ee02d338e486bc35dbaa007e569b54ef
jinalpateljinupatel312-coder <jinalpateljinupatel312@gmail.com>
2026-01-20 20:42:09 +0530

    Add files via upload

---
+++ assessment5 string handling/program3.py
@@ -0,0 +1,10 @@
+
+a = input(input("Enter first number:" ))
+b = input(input("Enter second number: "))
+
+if a > b:
+    print("Larger number is:", a)
+elif b > a:
+    print("Larger number is:", b)
+else:
+    print("Both numbers are equal")
