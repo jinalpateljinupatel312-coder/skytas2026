8daf918b963bce6b6d0af2c9614c644e662dc9b1
jinalpateljinupatel312-coder <jinalpateljinupatel312@gmail.com>
2026-01-20 11:51:36 +0530

    Delete prog7.py

--- prog7.py
+++
@@ -1,7 +0,0 @@
-sentence = input("Enter a sentence: ")
-word = input("Enter a word to search for: ")
-
-if word.lower() in sentence.lower():
-    print(f"The word '{word}' is present in the sentence.")
-else:
-    print(f"The word '{word}' is NOT present in the sentence.")
