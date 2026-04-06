de1993352f2698d172bf98312df44f3978e9413a
jinalpateljinupatel312-coder <jinalpateljinupatel312@gmail.com>
2026-04-06 09:10:44 +0530

    Delete dotnet assessment /dotnet  directory

--- dotnet assessment /dotnet /ASSESSMENT8/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace EFCoreCrudRepo.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
